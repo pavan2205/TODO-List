1be5b4f1a7cfdb26a86e4b1fd7d6ba6ad6057d0e
Pavan <pavannaik2205@gmail.com>
2022-06-07 19:35:15 +0530

    updated app.js

--- todoList/app.js
+++ todoList/app.js
@@ -10,24 +10,6 @@ app.use(bodyparser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
     var today=new Date();
-    // var day="";
-    // switch(today.getDay()){
-    //     case 0:day='Sunday';
-    //     break;
-    //     case 1:day='Monday';
-    //     break;
-    //     case 2:day='Tuesday';
-    //     break;
-    //     case 3:day='Wednesday';
-    //     break;
-    //     case 4:day='Thursday';
-    //     break;
-    //     case 5:day='Friday';
-    //     break;
-    //     case 6:day='Saturday';
-    //     break;
-    // }
-
     var options={
         weekday:"long",day:"numeric",month:"long"
     }
